29ebfbbd7e9ed1ed9b8e0a6b0552ede729902ce0
ckoh27Punahou <ckoh27@punahou.edu>
2024-03-12 16:09:35 -1000

    Update Robot.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -24,6 +24,8 @@ public class Robot extends OpMode {
 
     @Override
     public void loop() {
+
+        //Mecanum Code
         double y = -gamepad1.left_stick_y;
         double x = gamepad1.left_stick_x * 1.1; // 1.1 is for minor errors in strafing to make sure values are true
         double rx = gamepad1.right_stick_x;
@@ -39,5 +41,13 @@ public class Robot extends OpMode {
         backLeft.setPower(backLeftPower);
         frontRight.setPower(frontRightPower);
         backRight.setPower(backRightPower);
+
+        //Turret
+
+        double turretRotation = gamepad2.right_stick_x;
+
+        //Slides
+
+
     }
 }
