139719f4bfdc0e9f69841d8ba2819c73986a3c54
ckoh27Punahou <ckoh27@punahou.edu>
2024-03-13 11:03:56 -1000

    Update Autonomous.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous.java
@@ -4,7 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
-@com.qualcomm.robotcore.eventloop.opmode.Autonomous
+@com.qualcomm.robotcore.eventloop.opmode.Autonomous(name="AutonomousSkeleton")
 public class Autonomous extends OpMode {
 
     DcMotor frontLeft = hardwareMap.dcMotor.get("frontLeft");
